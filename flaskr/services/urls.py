b911e7eb0d7755efde3aa38edb1a78b7bb426420
Daniel Shogbon <ishogbon@gmail.com>
2024-09-15 17:58:29 +0100

    updated sites info to be fetched directly from mongo

--- flaskr/services/urls.py
+++ flaskr/services/urls.py
@@ -12,28 +12,46 @@ class UrlService:
     def __init__(self, db_client):
         self._db_client = db_client
 
-    def get_url_to_use(self):
+    def get_url_data_to_use(self, site_data):
+        links_data = site_data.get(
+            "LINKS", {}
+        )  # Extract LINKS data or default to empty dict
+
         return {
             "page_url": "",
-            "page_content_element": current_app.config["PAGE_CONTENT_ELEMENT"],
-            "related_articles_elements": current_app.config[
-                "RELATED_ARTICLES_ELEMENTS"
-            ],
-            "page_clicks": random.randint(
-                current_app.config["PAGE_CLICKS_MIN"],
-                current_app.config["PAGE_CLICKS_MAX"],
+            "page_content_element": links_data.get(
+                "page_content_element", current_app.config["PAGE_CONTENT_ELEMENT"]
             ),
-            "vignette_close_ad_elements": current_app.config[
-                "VIGNETTE_CLOSE_AD_ELEMENTS"
-            ],
-            "vignette_open_ad_elements": current_app.config[
-                "VIGNETTE_OPEN_AD_ELEMENTS"
-            ],
-            "in_page_open_ad_link_elements": current_app.config[
-                "IN_PAGE_OPEN_AD_LINK_ELEMENTS"
-            ],
-            "maximum_no_of_ads": current_app.config["MAXIMUM_NO_OF_ADS"],
-            "proxy_domain_whitelists": current_app.config["PROXY_DOMAIN_WHITELISTS"],
+            "related_articles_elements": links_data.get(
+                "related_articles_elements",
+                current_app.config["RELATED_ARTICLES_ELEMENTS"],
+            ),
+            "page_clicks": random.randint(
+                links_data.get(
+                    "page_clicks_min", current_app.config["PAGE_CLICKS_MIN"]
+                ),
+                links_data.get(
+                    "page_clicks_max", current_app.config["PAGE_CLICKS_MAX"]
+                ),
+            ),
+            "vignette_close_ad_elements": links_data.get(
+                "vignette_close_ad_elements",
+                current_app.config["VIGNETTE_CLOSE_AD_ELEMENTS"],
+            ),
+            "vignette_open_ad_elements": links_data.get(
+                "vignette_open_ad_elements",
+                current_app.config["VIGNETTE_OPEN_AD_ELEMENTS"],
+            ),
+            "in_page_open_ad_link_elements": links_data.get(
+                "in_page_open_ad_link_elements",
+                current_app.config["IN_PAGE_OPEN_AD_LINK_ELEMENTS"],
+            ),
+            "maximum_no_of_ads": links_data.get(
+                "maximum_no_of_ads", current_app.config["MAXIMUM_NO_OF_ADS"]
+            ),
+            "proxy_domain_whitelists": site_data.get(
+                "proxy_domain_whitelists", current_app.config["PROXY_DOMAIN_WHITELISTS"]
+            ),
         }
 
     def precompute__weights(self):
@@ -56,7 +74,7 @@ class UrlService:
                     "Cummulative sum of url weight can't be greater than or equal to 1"
                 )
             if len(site_urls) > 0:
-                site_weights.append((site, site_weight))
+                site_weights.append((site, site_weight, doc))
                 equal_weight = (1.0 - total_url_weight) / len(
                     [url for url in site_urls if len(url) == 1]
                 )
@@ -68,7 +86,10 @@ class UrlService:
                 weighted_urls[site] = [(url, weight) for url, weight in site_urls]
 
         total_weight = sum(weight for _, weight in site_weights)
-        site_weights = [(site, weight / total_weight) for site, weight in site_weights]
+        site_weights = [
+            (site, (weight / total_weight), site_data)
+            for site, weight, site_data in site_weights
+        ]
         return weighted_urls, site_weights
 
     def get_weighted_data(self, use_cache=True):
@@ -114,9 +135,10 @@ class UrlService:
             return "No site available"
 
         urls_for_site = [(url, weight) for url, weight in url_weights[site]]
-        return self.pick_weighted_item(urls_for_site)
+        return self.pick_weighted_item(urls_for_site), site[2]
 
     def fetch_random_url(self):
-        link_to_use = self.get_url_to_use()
-        link_to_use["page_url"] = self.get_weighted_url()
+        link_to_use, site_data = self.get_weighted_url()
+        link_data_to_use = self.get_url_data_to_use(site_data)
+        link_data_to_use["page_url"] = link_to_use
         return link_to_use
