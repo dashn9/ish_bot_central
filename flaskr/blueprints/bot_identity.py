040f70c31fb7df3611aab0a2f2e597cff94ee717
Daniel Shogbon <ishogbon@gmail.com>
2024-11-22 22:33:03 +0100

    fixed dup cookie upload to local storage

--- flaskr/blueprints/bot_identity.py
+++ flaskr/blueprints/bot_identity.py
@@ -69,8 +69,9 @@ def update_cookies(identity_id: int):
     cookies = identity_service.update_cookies(identity_id, cookies)
     return jsonify(cookies)
 
+
 @identity_bp.route("/<int:identity_id>/local-storage/", methods=["PUT"])
-def update_cookies(identity_id: int):
+def update_local_storage(identity_id: int):
     local_storage = request.json
     identity_service = IdentityService(db_client=get_db())
     local_storage = identity_service.update_local_storage(identity_id, local_storage)
